ae24f1eafcba721052ee6e1e0e2d90be9e4c3603
linjunhang <1650151130@qq.com>
2023-04-10 17:09:32 +0800

    thread git

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("aaaaa");
+        System.out.println("linjunhangjiayou");
     }
 }
